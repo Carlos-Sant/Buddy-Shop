482ed65f2a271b095c742308231681be25f3040d
poblete <fr.pobletec@duocuc.cl>
2023-04-24 10:40:21 -0400

    creación carpeta js y modificación index_u

---
+++ js/script.js
@@ -0,0 +1,10 @@
+var xhttp= new XMLHttpRequest(); 
+xhttp.onreadystatechange = function (){
+if (this.readyState ==4 && this.status == 200){
+    document.getElementById('menu-superior').innerHTML
+    = this.responseText; 
+}
+}
+
+xhttp.open('GET','menu-superior.html',true);
+xhttp.send();
