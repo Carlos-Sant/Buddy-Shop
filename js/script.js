b077b56a9cf3522a93330feb6f0496ba9589f372
Carlos Santivañez Huerta <ca.santivanez@duocuc.cl>
2023-05-06 02:32:23 -0400

    subiendo footer

--- js/script.js
+++ js/script.js
@@ -8,3 +8,14 @@ if (this.readyState ==4 && this.status == 200){
 
 xhttp.open('GET','menu-superior.html',true);
 xhttp.send();
+
+var xhttp= new XMLHttpRequest(); 
+xhttp.onreadystatechange = function (){
+if (this.readyState ==4 && this.status == 200){
+    document.getElementById('footer').innerHTML
+    = this.responseText; 
+}
+}
+
+xhttp.open('GET','footer.html',true);
+xhttp.send();
